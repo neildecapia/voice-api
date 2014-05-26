4ef9700ee0e18c0b37714430de543c8d055854b1
Neil L. Decapia <neildecapia@gmail.com>
2014-05-27 00:52:04 +0800

    Moving Asterisk client logs into its own file

--- config/initializers/client.rb
+++ config/initializers/client.rb
@@ -1,13 +1,8 @@
 require 'clients/asterisk'
 
 Clients::Asterisk.configure do
-
-  config.logger = Rails.logger
-
   config.env = Rails.env
-
   config.path = Rails.root.join('config', 'asterisk.yml').to_s
-
 end
 
 Rails.application.config.client = Clients::Asterisk.client
--- lib/clients/asterisk/config.rb
+++ lib/clients/asterisk/config.rb
@@ -12,4 +12,17 @@ class Clients::Asterisk::Config
     @call_options ||= connection_options['calls']
   end
 
+  def logger
+    @logger ||=
+      begin
+        log = File.open Rails.root.join('log', 'asterisk.log'), 'a'
+        log.binmode
+        log.sync = Rails.configuration.autoflush_log
+
+        ActiveSupport::Logger.new(log).tap do |logger|
+          logger.level = Rails.logger.level
+        end
+      end
+  end
+
 end
