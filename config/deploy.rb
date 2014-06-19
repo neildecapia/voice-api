8036b66f98303e922607c8b099fddeb9fe9fe330
Neil L. Decapia <neildecapia@gmail.com>
2014-06-20 02:02:38 +0800

    Linking to config/carrierwave.yml on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/asterisk.yml config/assets.yml config/unicorn.rb}
+set :linked_files, %w{config/database.yml config/secrets.yml config/asterisk.yml config/carrierwave.yml config/assets.yml config/unicorn.rb}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 load 'config/deploy/extensions/rbenv.rb'
